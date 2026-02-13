961405bc3878dcbd8c573e1d98aaaac6db221387
Giancarlo Benedetti <6262557@cegepmontpetit.ca>
2026-02-13 16:48:05 -0500

    AAA

--- ngPollingEtSignalR/src/app/polling/polling.component.ts
+++ ngPollingEtSignalR/src/app/polling/polling.component.ts
@@ -53,8 +53,7 @@ export class PollingComponent implements OnInit {
   async updateTasks() {
     // TODO: Faire une première implémentation simple avec un appel au serveur pour obtenir la liste des tâches
     // TODO: UNE FOIS QUE VOUS AVEZ TESTER AVEC DEUX CLIENTS: Utiliser le polling pour mettre la liste de tasks à jour chaque seconde
-    let x = await lastValueFrom(this.http.get<any>(this.apiUrl + 'UselessTasks/GetAll'));
-    console.log(x);
+    this.tasks = await lastValueFrom(this.http.get<any>(this.apiUrl + 'UselessTasks/GetAll'));
     setTimeout(() => {this.updateTasks()}, 1000);
   }
 }
